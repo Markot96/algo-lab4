7ca32436135d210b16193f030fa701623c48d11f
Marko <martopole@gmail.com>
2020-12-17 17:31:02 +0200

    added more tests

--- wchain_test.py
+++ wchain_test.py
@@ -1,13 +1,38 @@
 import unittest
 
-from lab4.wchain import WChain
+from wchain import WChain
 
 
 class WChainTest(unittest.TestCase):
 
-    def test_chain(self):
-        w = WChain()
-        self.assertEqual(6, w.count_chains("wchain.in"))
+    def test_chain1(self):
+        w = WChain("wchain1.in")
+        self.assertEqual(2, w.count_chains())
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_chain2(self):
+        w = WChain("wchain2.in")
+        self.assertEqual(3, w.count_chains())
+
+    def test_chain3(self):
+        w = WChain("wchain3.in")
+        self.assertEqual(1, w.count_chains())
+
+    def test_chain4(self):
+        w = WChain("wchain4.in")
+        self.assertEqual(4, w.count_chains())
+
+    def test_chain5(self):
+        w = WChain("wchain5.in")
+        self.assertEqual(3, w.count_chains())
+
+    def test_chain6(self):
+        w = WChain("wchain6.in")
+        self.assertEqual(5, w.count_chains())
+
+    def test_chain7(self):
+        w = WChain("wchain7.in")
+        self.assertEqual(4, w.count_chains())
+
+
+if __name__ == '__main__':
+    unittest.main()
